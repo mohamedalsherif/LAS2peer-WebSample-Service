41aeb43c911f51e12104e691924a0d5c6ae47fdd
Mohamed <mohamedalsherif@hotmail.com>
2013-11-15 13:05:53 +0100

    Initial project commit

---
+++ WebContent/js/SampleWebService.js
@@ -0,0 +1,186 @@
+	//Private Porperties
+				var LAS2PEERHOST = "http://localhost:8080/";
+		var LAS2PEERSERVICENAME = "i5.las2peer.services.webSampleService.WebSampleService";
+		var LAS2peerClient; 
+		/**
+		* Handles submission of the Login Form. Tries to login to LAS2peer and shows the Methods selection on success.
+		*/
+		var login_form_submit = function()
+		{
+			var username = (document.getElementById("cs_username")).value,
+				password = (document.getElementById("cs_password")).value;
+			if(username != "" && password!="")
+			{
+				if(LAS2peerClient.getStatus() != "loggedIn")
+				{
+					LAS2peerClient.login(username, password, LAS2PEERHOST, "WebSampleServiceFrontend");
+				}
+			}
+		};
+		function loggedInSuccessfully()
+		{
+			document.getElementById("cs_loginform").style.visibility="hidden";
+			document.getElementById("cs_logout").style.visibility="visible";
+			document.getElementById("selectMethod").style.visibility="visible";
+			LAS2peerClient.invoke(LAS2PEERSERVICENAME, "getMethods", "", function(status, result) 
+			{
+				if(status == 200 || status == 204) 
+				{
+					populateComboBox(result.value);
+				} 
+				else 
+				{
+					console.log("Error! Message: " + result);
+				}
+			});
+		}
+		
+		
+		function populateComboBox(methodNamesArray)
+		{
+			var select = document.getElementById("selectMethod");
+			select.innerHTML="";
+			for(var i = 0; i < methodNamesArray.length; i++) 
+			{
+			    var opt = methodNamesArray[i];
+			    var el = document.createElement("option");
+			    el.textContent = opt;
+			    el.value = opt;
+			    select.appendChild(el);
+			}
+		}
+		
+		function logout()
+		{
+			LAS2peerClient.logout();
+			document.getElementById("cs_loginform").style.visibility="visible";
+			document.getElementById("cs_logout").style.visibility="hidden";
+			document.getElementById("selectMethod").style.visibility="hidden";
+			var div = document.getElementById("textBoxesDiv");
+			div.innerHTML = '';
+		}
+		
+		function ComboBoxChange()
+		{
+		    var e = document.getElementById("selectMethod");//get the combobox
+		    var selMethod = e.options[e.selectedIndex].value;//get selected value
+			invocationArguments = [ {
+				"type" : "String",
+				"value" : selMethod
+			}];
+		    LAS2peerClient.invoke(LAS2PEERSERVICENAME, "getParameterTypesOfMethod", invocationArguments, function(status, result) 
+		    {
+				if(status == 200 || status == 204) 
+				{	
+					AddTextBoxes(result.value);
+				} 
+				else 
+				{
+					console.log("Error! Message: " + result);
+				}
+			});                 
+		}
+		
+		
+		function InvokeButtonClicked()
+		{
+			var textBoxes=document.getElementsByClassName("argumentTextBoxes");
+			var invocationArguments = [];
+			for(var i=0;i<textBoxes.length;i++)
+				{
+					var elem =document.getElementById("argumentTextBoxes"+i);
+					var val =elem.value;
+					var parameterI={};
+					parameterI.type = elem.placeholder;
+					parameterI.value = val;
+					invocationArguments.push(parameterI);
+				}
+			
+			   var e = document.getElementById("selectMethod");//get the combobox
+			    var selMethod = e.options[e.selectedIndex].value;//get selected value
+			  LAS2peerClient.invoke(LAS2PEERSERVICENAME, selMethod, invocationArguments, function(status, result) 
+			{
+					if(status == 200 || status == 204) 
+					{
+						console.log("Message Invoked successfully");
+						ShowResult(result);
+					} 
+					else 
+					{
+						console.log("Error! Message: " + result);
+						var resultCorrectFormat={};
+						resultCorrectFormat.value = result;
+						ShowResult(resultCorrectFormat);
+					}
+				}); 
+		}
+		
+		
+		function ShowResult(result)
+		{
+			var text = document.createElement('p');
+			if(result.type=="none")
+			{
+				text.innerHTML = "(void) Method";
+			}
+			else
+			{
+				text.innerHTML = result.value;
+			}
+
+			var div = document.getElementById("textBoxesDiv");
+			div.appendChild(text);
+		}
+		
+		function AddTextBoxes(parameters)
+		{
+			var div = document.getElementById("textBoxesDiv");
+			div.innerHTML = '';
+			for(var i = 0; i < parameters.length; i++) 
+			{
+				var input = document.createElement('input'); 
+				input.type = "text"; 
+				input.id="argumentTextBoxes"+i;
+				input.className="argumentTextBoxes";
+				input.placeholder=parameters[i];
+				div.appendChild(input);
+			}
+			
+			var button = document.createElement('input');
+			button.type = "button"; 
+			button.value="invoke Method";
+			button.onclick=InvokeButtonClicked;
+			div.appendChild(button);
+		}
+		
+		window.onload = function() 
+		{
+			document.getElementById("cs_logout").style.visibility="hidden";
+			document.getElementById("selectMethod").style.visibility="hidden";
+			LAS2peerClient = new LasAjaxClient("WebSampleService", function(statusCode, message) 
+					{
+						switch(statusCode) 
+							{
+								case Enums.Feedback.LoginSuccess:
+									console.log("Login successful!");
+									loggedInSuccessfully();
+									break;
+								case Enums.Feedback.LogoutSuccess:
+									console.log("Logout successful!");
+									break;
+								case Enums.Feedback.LoginError:
+								case Enums.Feedback.LogoutError:
+									console.log("Login error: " + statusCode + ", " + message);
+									break;
+								case Enums.Feedback.InvocationWorking:
+								case Enums.Feedback.InvocationSuccess:
+								case Enums.Feedback.Warning:
+								case Enums.Feedback.PingSuccess:
+									break;
+								default:
+									console.log("Unhandled Error: " + statusCode + ", " + message);
+								break;
+							}
+					});
+		};
+		
---
+++ java/i5/las2peer/services/webSampleService/WebSampleService.java
@@ -0,0 +1,107 @@
+package i5.las2peer.services.webSampleService;
+
+import i5.las2peer.api.Service;
+import i5.las2peer.security.UserAgent;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WebSampleService extends Service 
+{
+	private int randomNumber=0;
+	
+	/**
+	 * used in the JS Class to get all the Methods declared in this class  
+	 * @return all this class' declared methods as a list of strings
+	 */
+	public String[] getMethods()
+	{
+		List<String> allMethods = new ArrayList<String>();
+		for ( Method m : WebSampleService.class.getDeclaredMethods()) 
+		{
+			if ( Modifier.isPublic ( m.getModifiers()) 
+				&& !Modifier.isStatic(m.getModifiers()) 
+				) 
+			{
+				allMethods.add(m.getName());
+			}
+		}
+		
+		return allMethods.toArray(new String[allMethods.size()]);
+	}
+	
+	/**
+	 * used in the JS Class to show the parameters of the given method 
+	 * @param methodName the method name  
+	 * @return A list with all parameters of the given input method
+	 */
+	public String[] getParameterTypesOfMethod(String methodName)
+	{	
+		for ( Method m : WebSampleService.class.getDeclaredMethods()) 
+		{
+			if ( Modifier.isPublic ( m.getModifiers()) 
+					&& !Modifier.isStatic(m.getModifiers()) 
+					&& m.getName().equals(methodName) )
+					
+			{
+					Class[] parameterTypesClasses = m.getParameterTypes();
+					String[] parameterTypes=new String[parameterTypesClasses.length];
+					for(int i=0;i<parameterTypes.length;i++)
+					{
+						parameterTypes[i]=parameterTypesClasses[i].getSimpleName();
+					}
+					
+					if(parameterTypesClasses.length!=0)
+					{
+						return parameterTypes;
+					}
+					
+					return null;
+			}
+		}
+		 return new String[]{"No such method declared in the service " + WebSampleService.class.getName() + "."}; 
+	}
+	/**
+	 * test Method that always returns true
+	 * @return true
+	 */
+	public boolean boolTestMethod() 
+	{
+		return true;
+	}
+	
+	/**
+	 * a test Method that returns a formatted string that contains the given string parameter and the user agents login name 
+	 * @param firstMessage the first part of the string that will be returned  
+	 * @param ignoredNumber an unused integer demonstrating a method with two different parameters 
+	 * @return a formatted string that contains the given input string and the agents login name
+	 */
+	public String stringIntTestMethod(String firstMessage, int ignoredNumber) 
+	{
+		UserAgent sendingAgent = (UserAgent) this.getActiveAgent(); 
+		return String.format("%s %s", firstMessage, sendingAgent.getLoginName()); 
+		// this is the User Agent that was used to log in LAS2Peer
+	}
+	
+	/**
+	 * increments the private randomNumber by 1 
+	 * @param doNothing an ignored string
+	 */
+	public void changeRandomNumber(String doNothing)
+	{
+		randomNumber++;
+	}
+	/**
+	 * a get Method that returns the randomNumber added to all the input integers
+	 * @param a first number
+	 * @param b second number 
+	 * @param c third number 
+	 * @return the sum of all the input numbers added to the private instance variable randomNumber
+	 */
+	public int getRandomNumber(int a,int b,int c)
+	{
+		return randomNumber+a+b+c;
+	}
+}
